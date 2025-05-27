0f03adeb8236dd6dd8a40cb652b7a8956e4ee790
Lizeth <lizeth.mcx@gmail.com>
2025-05-27 16:11:38 -0600

    Taller entregable 7

---
+++ 3.7Node/mi-exploracion-espacial/planetas.js
@@ -0,0 +1,34 @@
+const planetas = [
+{
+    nombre: "Titán",
+    descripcion: "La luna más grande de Saturno, con lagos de metano.",
+    descubiertoEn: "1655"  
+},
+{
+    nombre: "Próxima Centauri b",
+    descripcion: "Un exoplaneta rocoso en la zona habitable de su estrella.",
+    descubiertoEn: "2016"
+},
+{
+    nombre: "Kepler-452b",
+    descripcion: "Un exoplaneta potencialmente habitable que orbita una estrella similar al Sol.",
+    descubiertoEn: "2015"
+},
+{
+    nombre: "TRAPPIST-1e",
+    descripcion: "Uno de los siete planetas rocosos del sistema TRAPPIST-1, en la zona habitable.",
+    descubiertoEn: "2017"
+},
+{
+    nombre: "HD 209458 b",
+    descripcion: "El primer exoplaneta en el que se detectó una atmósfera.",
+    descubiertoEn: "1999"
+},
+{
+    nombre: "Gliese 581c",
+    descripcion: "Un planeta rocoso ubicado en la zona habitable de una enana roja.",
+    descubiertoEn: "2007"
+}
+];
+
+module.exports = planetas;
